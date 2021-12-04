f943a7fafa706de263cb3a8d2afae2d59a632889
Eric Dee <atallbirdmaker@protonmail.com>
2021-12-03 20:37:07 -0700

    Proxy server init.

---
+++ index.js
@@ -0,0 +1,84 @@
+import path from "path";
+import express from "express";
+import axios from "axios";
+import cron from "node-cron";
+import fs from "fs";
+
+if (!fs.existsSync("./api_access_files/json")) {
+  fs.mkdirSync("./api_access_files/json", { recursive: true });
+}
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname + "/public_files/")));
+
+var wordpress_posts;
+
+/* 
+    The purpose of this call is to periodically update behind the scenes, and let the JavaScript + HTML do the work 
+    Justification: The wordpress API can be slow.
+*/
+
+//  TODO: Store images in a database, and replace them as needed.
+
+/* 
+    APPLICATION: This website serves as a proxy with minimal external load times, at the cost of being slightly
+                 behind its API endpoints. 
+*/
+
+async function server_backlog() {
+  await axios
+    .get("https://ericdee.blog/?rest_route=/wp/v2/posts&_embed")
+    .then((response) => {
+      fs.writeFileSync(
+        "./api_access_files/json/wordpress_posts.json",
+        JSON.stringify(response.data)
+      );
+      console.log("A WordPress JSON file has been written.");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  wordpress_posts = fs.readFileSync(
+    "./api_access_files/json/wordpress_posts.json",
+    "utf-8"
+  );
+}
+
+server_backlog();
+
+/**************************************************************************************************************************/
+
+app.listen(port, () => {
+  console.log(`This server is available on port ${port}.`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public_files/html/index.html"));
+});
+
+app.get("/wordpress_all_posts", (req, res) => {
+  res.send(wordpress_posts);
+  console.log(
+    `A get request has been received/sent for wordpress posts from ${
+      req.header("x-forwarded-for") || req.connection.remoteAddress
+    }.`
+  );
+});
+
+/*
+  * * * * * *
+  | | | | | |
+  | | | | | day of week
+  | | | | month
+  | | | day of month
+  | | hour
+  | minute
+  second ( optional )
+
+  5 Asterisks will run a task once a minute.
+*/
+cron.schedule("* * * * *", function () {
+  server_backlog();
+});
---
+++ public_files/scripts/loads_wordpress_json.js
@@ -0,0 +1,22 @@
+let content_variability = document.querySelector(".content_variability");
+
+!(async function () {
+  let data = await fetch("/wordpress_all_posts")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((post) => {
+        let postOutline = document.createElement("div");
+        postOutline.classList.add("wordpress_post_outlines");
+        let postTitle = document.createElement("h2");
+        postTitle.innerHTML = post.title.rendered;
+        postOutline.appendChild(postTitle);
+        let postContent = document.createElement("p");
+        postContent.innerHTML = post.content.rendered;
+        postOutline.appendChild(postContent);
+        content_variability.appendChild(postOutline);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+})();
